bd0867e12d060f748444f6823af36b44c50f56ae
Lei <gaoleilucky@gmail.com>
2014-10-14 10:33:24 +0800

    Change wit c++11, Lab 1 passed the test

--- lock_server.cc
+++ lock_server.cc
@@ -11,7 +11,7 @@ lock_server::lock_server():
 {
 }
 
-lock_protocol::status
+  lock_protocol::status
 lock_server::stat(int clt, lock_protocol::lockid_t lid, int &r)
 {
   lock_protocol::status ret = lock_protocol::OK;
@@ -19,5 +19,56 @@ lock_server::stat(int clt, lock_protocol::lockid_t lid, int &r)
   r = nacquire;
   return ret;
 }
+
+  lock_protocol::status
+lock_server::acquire(int clt,lock_protocol::lockid_t lid, int &r)
+{
+
+  std::unique_lock<std::mutex> guard(table_mutex_);
+  auto it = lock_table_.find(lid);
+  if (it == lock_table_.end()) {
+    it = lock_table_.find(lid);
+    if (it == lock_table_.end()) {
+      //no this kind of lock, insert.
+      auto lockstatus = klocked;
+      auto p = std::make_pair(lid,lockstatus);
+      lock_table_.insert(p);
+    }
+  }
+  else {
+    if (lock_table_[lid] == klocked) {
+      while(locked) {
+        cond.wait(guard);
+      }
+      lock_table_[lid] = klocked;
+    }
+
+    else {
+      lock_table_[lid] = klocked;
+    }
+  }
+  locked = true;
+
+  return 0;
+
+}
+
+
+  lock_protocol::status
+lock_server::release(int clt,lock_protocol::lockid_t lid, int &r)
+{
+
+  std::unique_lock<std::mutex> guard(table_mutex_);
+  auto it = lock_table_.find(lid);
+  if (it != lock_table_.end()) {
+    lock_table_[lid] = kfree;
+    locked = false;
+    cond.notify_one();
+
+  }
+  return 0;
+}
+
+
 
 
